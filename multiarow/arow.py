d487883b06ec0b617440079b99ddf6c45d555e01
Yuki Hayashi <yuki@ikkyotech.com>
2015-07-07 09:29:05 +0900

    break down into into predict() and classify()

--- multiarow/arow.py
+++ multiarow/arow.py
@@ -61,34 +61,39 @@ class AROW(object):
             if y != 0:
                 self._update(l, x, y, self.w[l], self.v[l])
 
-    def classify(self, x, detailed=False):
+    def classify(self, x):
         '''Perfom classification based on the current weights
         Parameters
         ----------
         x : array-like
             Vector instance to predict
-        detailed : boolean
-            If Ture, scores are returned with predicted classes.
-            Defaults to False.
         Returns
         -------
         label : str
-            In case detailed == False. The most probable class label
+            The most probable class label. Returns None if it's not trained.
+        '''
+        pred = self.predict(x)
+        if pred is not None:
+            return pred[0][0]
+
+    def predict(self, x):
+        '''Perfom classification based on the current weights
+        Parameters
+        ----------
+        x : array-like
+            Vector instance to predict
+        Returns
+        -------
         scores : [(label0, score0), (label1, score1), ...]
-            In case detailed == True. List of labels with prediction scores.
+            List of labels with prediction scores.
+            Returns None if it's not trained.
         '''
         if len(self.w) == 0:
             return None
-        if detailed:
-            return sorted(
-                [(l, self._compute(x, w)) for l, w in self.w.items()],
-                key=lambda a: a[1],
-                reverse=True)
-        else:
-            l, c = max(
-                [(l, self._compute(x, w)) for l, w in self.w.items()],
-                key=lambda a: a[1])
-            return l
+        return sorted(
+            [(l, self._compute(x, w)) for l, w in self.w.items()],
+            key=lambda a: a[1],
+            reverse=True)
 
     def list_label(self):
         '''List up all the current labels.
